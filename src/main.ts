275e7e209831fd5dfe7f478c42dc8e04ffec0d38
Takashi Tamura <tamuratak@users.noreply.github.com>
2020-08-13 08:26:23 +0900

    edit

--- src/main.ts
+++ src/main.ts
@@ -2,33 +2,55 @@ import * as vsls from 'vsls/vscode'
 import * as vscode from 'vscode'
 
 
-export function workspaceFolder() {
-    const ret = vscode.workspace.workspaceFolders?.[0]
-    if (!ret) {
-        throw new Error()
-    }
-    return ret
-}
+export class VslsFileSystem {
+    private readonly _vslsApi: Promise<vsls.LiveShare | null>
+    service: vsls.SharedService | null
 
-export async function registerFileSystemProviderOnHost() {
-    const vslsApi = await vsls.getApi()
-    if (!vslsApi) {
-        return
+    constructor() {
+        this._vslsApi = vsls.getApi()
     }
-    const service = await vslsApi.shareService('vslsfs')
-    if (!service) {
-        return
+
+    async vslsApi() {
+        const api = await this._vslsApi
+        if (!api) {
+            throw new Error()
+        }
+        return api
     }
-    service.onRequest('readFile', async ([uriStr]: [unknown]) => {
+
+    workspaceFolder() {
+        const ret = vscode.workspace.workspaceFolders?.[0]
+        if (!ret) {
+            throw new Error()
+        }
+        return ret
+    }
+
+    async uriToPath(uriStr: unknown) {
+        const vslsApi = await this.vslsApi()
         if (typeof uriStr !== 'string') {
-            return
+            throw new Error()
         }
         const uri = vscode.Uri.parse(uriStr)
-        const path = vslsApi.convertSharedUriToLocal(uri)
-        const data = await vscode.workspace.fs.readFile(path)
-        const buf = Buffer.from(data)
-        return buf
-    })
+        const path = vslsApi?.convertSharedUriToLocal(uri)
+        return path
+    }
+
+    async registerFileSystemProviderOnHost() {
+        const vslsApi = await this.vslsApi()
+        const service = await vslsApi.shareService('vslsfs')
+        this.service = service
+        if (!service) {
+            throw new Error()
+        }
+        service.onRequest('readFile', async ([uriStr]: [unknown]) => {
+            const path = await this.uriToPath(uriStr)
+            const data = await vscode.workspace.fs.readFile(path)
+            const buf = Buffer.from(data)
+            return buf
+        })
+    }
+
 }
 
 
